fcd75d07996b4a29ededa8f40f2bd6a9e827a21b
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-08-13 10:17:34 -0500

    Tarea 1 Terminada

---
+++ src/CuentaBancaria.java
@@ -0,0 +1,36 @@
+public class CuentaBancaria {
+    private String numeroCuentas;
+    private String titular;
+    private double saldo;
+
+    public CuentaBancaria(String numeroCuentas, String titular, double saldo){
+        this.numeroCuentas = numeroCuentas;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public String getNumeroCuentas() {
+        return numeroCuentas;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    public String describir(){
+        return "Cuenta " + numeroCuentas + "\nTitular "+ titular +"\nSaldo: $" + saldo;
+    }
+    public double calcularComision(){
+        return 0.0;
+    }
+    public void realizarRetiro(double monto){
+        setSaldo(saldo-monto);
+    }
+}
