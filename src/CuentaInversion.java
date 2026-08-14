759e2a7d7a150bf192e143b22d196890353becbf
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-08-14 00:01:29 -0500

    Tarea 4 terminada

--- src/CuentaInversion.java
+++ src/CuentaInversion.java
@@ -3,7 +3,32 @@ public class CuentaInversion extends CuentaBancaria {
     private int plazoMeses;
     private double penalizacionRetiroAnticipado;
 
-    public CuentaInversion(String numeroCuentas, String titular, double saldo, double tasaAnual, int plazoMeses, double limiteSobregiro) {
+    public CuentaInversion(String numeroCuentas, String titular, double saldo, double tasaAnual, int plazoMeses, double penalizacionRetiroAnticipado) {
         super(numeroCuentas, titular, saldo);
+        this.tasaAnual = tasaAnual;
+        this.plazoMeses = plazoMeses;
+        this.penalizacionRetiroAnticipado = penalizacionRetiroAnticipado;
+    }
+
+    @Override
+    public String describir() {
+        return super.describir() + " | Plazo: " + plazoMeses + " meses" + " | Tasa Anual: " + tasaAnual + "%";
+    }
+
+    @Override
+    public double calcularComision() {
+        return penalizacionRetiroAnticipado;
+    }
+
+    @Override
+    public void realizarRetiro(double monto) {
+        double penalizacionDescuento = getSaldo() - monto - penalizacionRetiroAnticipado;
+        setSaldo(penalizacionDescuento);
+    }
+    public double calcularComision(int mesesTranscurridos) {
+        if (mesesTranscurridos >= plazoMeses) {
+            return 0.0;
+        }
+        return penalizacionRetiroAnticipado;
     }
 }
