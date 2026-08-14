6d228ffa70ebaafa1b75e8acffa28d45a4951254
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-08-14 00:58:37 -0500

    RTA a la pregunta

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,22 @@ public class Main {
 
         System.out.println(c1.getSaldo());
         System.out.println(c2.getSaldo());
+        System.out.println(c3.getSaldo());
     }
 }
+
+/*¿Por qué fue necesario usar CuentaBancaria como tipo de variable en lugar de cada tipo especifico?
+RTA: Porque CuentaBancaria es la clase padre de las demás clases.
+¿Qué vemtaja concreta me dio?
+RTA: Es que pude llamar con una sola cuenta todos los diferentes tipos de cuentas.
+=====================================================================================================
+El banco decide agregar un cuarto tipo de cuenta CuentaNomina con sus propias reglas de comisión y retiro.
+¿Tendrías que modificar el código de Main para que funcione con las variables que ya declaraste?
+RTA: NO
+¿Por qué?
+RTA: Por que la nueva cuenta ya estaria heredando lo de cuentaBancaria.
+======================================================================================================
+Si Main solo conoce el tipo CuentaBancaria y llama métodos que existen en el padre.
+¿Qué tendría que cambiar cuando aparece una hija nueva?
+RTA: Tecnicamente nada, porqué la nueva cuenta hereda los metodos y atributos de CuentaBancaria, además de que tendria sus propios atributos.
+En el main se seguiria manejando la misma estructura CuentaBancaria c4 = new cuentaNieva(sus parametros). etc.. etc.. etc..*/
